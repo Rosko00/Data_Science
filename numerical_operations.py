f39f9b32c029e01987f5e0e3ad057bab5aa5e233
Marián Gálik <marian.galik@gmail.com>
2022-09-15 23:37:26 +0200

    Create numerical_operations.py

---
+++ numerical_operations.py
@@ -0,0 +1,33 @@
+# addition
+addition_01 = (13 + 21)
+addition_02 = (13.6 + 21.7)
+print (addition_01)
+print (addition_02)
+
+# subtraction
+subtraction_01 = (21 - 13)
+subtraction_02 = (13.6 - 21.7)
+print (subtraction_01)
+print (subtraction_02)
+
+# multiplication
+multiplication_01 = (13 * 21)
+multiplication_02 = (13.6 * 21.7)
+print (multiplication_01)
+print (multiplication_02)
+
+# division
+division = (21 / 13)
+print (division)
+
+# integer division
+integer_division = (21 // 13)
+print (integer_division)
+
+# square root
+square_root = (21**21)
+print (square_root)
+
+# modulus
+modulus = (21 % 13)
+print (modulus)
