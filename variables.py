fec53f9341f979e53dd4a42cf6a2d11e0536b971
Marián Gálik <marian.galik@gmail.com>
2022-09-16 22:19:41 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+# variables 
+
+# Creating Variables
+name = "Marian"
+print (name)
+
+# numbers in variable name
+my_name_2 = "Galik"
+print (my_name_2)
+
+# Input function for user input from the console
+my_name = input ("Type your name:")
+print (my_name)
+input ("Type anything:")
+
+# addition variables
+number_of_apples = 20
+number_of_pears = 3.4
+print (number_of_apples + number_of_pears)
